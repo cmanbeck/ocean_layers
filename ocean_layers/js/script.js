fb39954ca03d4aec33596394b472a371a712e350
Courtney <manbeckcourtney@gmail.com>
2015-08-26 16:53:33 +0000

    parallax is working

---
+++ ocean_layers/js/script.js
@@ -0,0 +1,37 @@
+var layerInfo = {
+  "epipelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+  
+  "mesopelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+  "bathypelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+  "abyssopelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+  "hadalpelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+}
